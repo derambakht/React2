09fdfd417a93394aea1ff09b1a2e6e2962acb82a
Jahed Manesh <47even@gmail.com>
2019-12-20 11:42:09 +0330

    Modify AppJS

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ export default class App extends Component {
                 </div>
               </div>
             </div>
+            <hr />
           </>
         </HashRouter>
       </Provider>
